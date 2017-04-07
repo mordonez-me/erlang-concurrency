b6da9d32a313a19a8fa497b6130426a9165d6fc3
Marco Ordonez <mordonez.me@gmail.com>
2017-04-06 23:20:02 -0500

    Fix to add support to handle 2 requests at the same time

--- exercise1/palin.erl
+++ exercise1/palin.erl
@@ -1,5 +1,5 @@
 -module(palin).
--export([palindrome_check/0, is_palindrome/1, response_is_palindrome/1]).
+-export([serve/1, internal_server/1, is_palindrome/1, response_is_palindrome/1]).
 
 rem_punct(String) -> 
   lists:filter(fun(Ch) ->
@@ -22,13 +22,32 @@ is_palindrome(String) ->
 
 response_is_palindrome(String) ->
   case (is_palindrome(String)) of
-    true -> "es palindrome.";
-    false -> "no es palindrome."
+    true -> "is a palindrome.";
+    false -> "is not a palindrome."
   end.
 
-palindrome_check() ->
-  receive
-    Msg ->
-      io:format("Answer: ~s~n", [response_is_palindrome(Msg)]),
-    palindrome_check()
+frontend_server(Pid, Servers) ->
+  receive 
+    {check, Msg} ->
+      [Server | _] = Servers,
+      Server ! {Pid, check, Msg},
+      frontend_server(Pid, lists:reverse(Servers))
   end.
+
+internal_server(Index) ->
+  receive
+    {Pid, check, Msg} ->
+      Strings = [Msg, response_is_palindrome(Msg)],
+      ReturnMessage = string:join(Strings, " "),
+      io:format("Receiving message from in index ~B~n", [Index]),
+      Pid ! { result,  ReturnMessage },
+      internal_server(Index);
+    stop ->
+      io:format("Terminating internal server ~n", [])
+  end.
+
+serve(Pid) ->
+  io:write("Starting server ~n"),
+  Server_1 = spawn(palin, internal_server, [1]),
+  Server_2 = spawn(palin, internal_server, [2]),
+  frontend_server(Pid, [Server_1, Server_2]).
